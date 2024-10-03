2a4439ef49dc02a8a6f292f3decae105615313d8
unknown <honeyavutu@gmail.com>
2024-10-03 14:13:53 +0530

    Updated routing to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home";
 import SubmitStudent from "./pages/SubmitStudent";
@@ -8,20 +8,20 @@ import DeleteStudent from "./pages/DeleteStudent";
 import Login from "./pages/Login";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path={"/"} element={<Login/>}/>
-              <Route path={"dashboard"} element={<SharedLayout/>}>
-                  <Route index element={<Home/>}/>
-                  <Route path={"submit"} element={<SubmitStudent/>}/>
-                  <Route path={"get"} element={<GetStudent/>}/>
-                  <Route path={"update"} element={<UpdateStudent/>}/>
-                  <Route path={"delete"} element={<DeleteStudent/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
-  );
-}
-
-export default App;
+    return (
+        <Router>
+            <Routes>
+                <Route path={"/"} element={<Login/>}/>
+                <Route path={"dashboard"} element={<SharedLayout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path={"submit"} element={<SubmitStudent/>}/>
+                    <Route path={"get"} element={<GetStudent/>}/>
+                    <Route path={"update"} element={<UpdateStudent/>}/>
+                    <Route path={"delete"} element={<DeleteStudent/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    );
+  }
+  
+  export default App;
